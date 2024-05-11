0a4e8a47a82734d45b8fb8e8da237a35567b9f24
matheus-carrasco <matheuscarrasco02@gmail.com>
2024-05-11 15:56:04 -0300

    Obtendo um usuario por ID

--- src/main/java/com/matheuscarrasco/workshopmongo/services/UserService.java
+++ src/main/java/com/matheuscarrasco/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.matheuscarrasco.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.matheuscarrasco.workshopmongo.domain.User;
 import com.matheuscarrasco.workshopmongo.repositories.UserRepository;
+import com.matheuscarrasco.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,9 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
-
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));	
+	}
 }
