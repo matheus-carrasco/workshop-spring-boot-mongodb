b1f7253ff3cc6b58edcae539f6c385cb66656f9a
matheus-carrasco <matheuscarrasco02@gmail.com>
2024-05-14 19:52:37 -0300

    Obtendo Post por Id

---
+++ src/main/java/com/matheuscarrasco/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.matheuscarrasco.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.matheuscarrasco.workshopmongo.domain.Post;
+import com.matheuscarrasco.workshopmongo.repositories.PostRepository;
+import com.matheuscarrasco.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));	
+	}	
+}
